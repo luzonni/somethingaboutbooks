a3e22eefa844c71cb9fa624d416490403f20f017
luzonni <lucas.z.200375@gmail.com>
2024-09-01 16:09:09 -0300

    add routes and pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Route from './Route.jsx'
-import './index.css'
+import AppRoutes from './Routes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Route />
-  </StrictMode>,
+    <AppRoutes />
+  </StrictMode>
 )
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <>
+            <h1>Ola!</h1>
+        </>
+    )
+}
+
+export default Home
