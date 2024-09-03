dd25289def4bcd0dac21565173fda3f9f7d3b4fe
luzonni <lucas.z.200375@gmail.com>
2024-09-03 08:28:57 -0300

    there's no login page

--- src/components/MyHeader/index.jsx
+++ src/components/MyHeader/index.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import { ColorTwo } from "../../assets/Colors"
+import { ColorFour, ColorTwo } from "../../assets/Colors"
 import { NavLink } from "react-router-dom"
 
 const NavegationStyle = styled.nav`
@@ -20,6 +20,11 @@ const NavStyle = styled.ul`
     display: flex;
     gap: 1rem;
     align-items: center;
+    .item__link {
+        text-decoration: none;
+        font-size: 1.5rem;
+        color: ${ColorFour};
+    }
 `
 
 const MyHeader = () => {
@@ -29,10 +34,10 @@ const MyHeader = () => {
                 <img src="/imgs/logo.png" alt="Logo Site" />
             </LogoStyle>
             <NavStyle>
-                <li><NavLink to="/" >Home</NavLink> </li>
-                <li><NavLink to="/explorer">Explorer</NavLink></li>
-                <li><NavLink to="/mybookcase">My Bookcase</NavLink></li>
-                <li><NavLink to="/login">Login</NavLink></li>
+                <li><NavLink className="item__link" to="/" >Home</NavLink> </li>
+                <li><NavLink className="item__link" to="/explorer">Explorer</NavLink></li>
+                <li><NavLink className="item__link" to="/mybookcase">My Bookcase</NavLink></li>
+                <li><NavLink className="item__link" to="/login">Login</NavLink></li>
             </NavStyle>
         </NavegationStyle>
     )
