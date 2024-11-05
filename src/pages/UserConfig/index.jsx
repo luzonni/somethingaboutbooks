a7de99f1925bd30a4265b6808a9d1a828497e31b
Caio Renato <caiorenato@Caios-MacBook-Pro.local>
2024-11-05 09:12:51 -0300

    inicializacao, do projeto renato conjunto.

---
+++ src/pages/UserConfig/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./UserConfig.module.css"
+
+
+function UserConfig() {
+    return (
+        <section className={styles.container}>
+            <h1>Ola mundo!</h1>
+        </section>
+    )
+}
+
+export default UserConfig
